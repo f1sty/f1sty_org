da02625a36711270f7664e4263bf6cfb9f3fd31e
Yurii Skrynnykov <truef1s7@gmail.com>
2019-01-30 18:55:23 +0200

    Add padding to body element for better view at phones

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Site.MixProject do
     [
       app: :site,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
